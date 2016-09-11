cb00332bd0776d251559f13f1b7939e461dcb2f4
HanByul Yang <hanbyulyang@gmail.com>
2016-09-11 22:54:04 +0900

    added convergence test

--- codes/mysqrt/mysqrt/mysqrt.cpp
+++ codes/mysqrt/mysqrt/mysqrt.cpp
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     double x = 2.;
     double s = 1.;
 
+    int maxiter = 100;
+    double tol = 1.e-14;
     int i = 0;
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < maxiter; i++)
     {
         printf("Before iteration %d, s = %f\n", i, s);
+        double s0 = s;
         s = 0.5 * (s + x / s);
+
+        double delta_s = s - s0;
+        if (fabs(delta_s / x) < tol)
+            break;
     }
     printf("After %d iterations, s = %f\n", i, s);
 
