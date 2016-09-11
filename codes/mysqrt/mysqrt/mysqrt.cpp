0326f6eedc62d39ee55789ffa3ac60a377e84e80
HanByul Yang <hanbyulyang@gmail.com>
2016-09-11 23:20:57 +0900

    separates mysqrt() function with new file

--- codes/mysqrt/mysqrt/mysqrt.cpp
+++ codes/mysqrt/mysqrt/mysqrt.cpp
@@ -1,30 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-
-double mysqrt(double x, bool debug = false)
-{
-
-    double s = 1.;
-
-    int maxiter = 100;
-    double tol = 1.e-14;
-    int i = 0;
-    for (i = 0; i < maxiter; i++)
-    {
-        if (debug)
-            printf("Before iteration %d, s = %20.15f\n", i, s);
-        double s0 = s;
-        s = 0.5 * (s + x / s);
-
-        double delta_s = s - s0;
-        if (fabs(delta_s / x) < tol)
-            break;
-    }
-    if (debug)
-        printf("After %d iterations, s = %20.15f\n", i, s);
-
-    return s;
-}
+#include "mymath.h"
 
 int main()
 {
