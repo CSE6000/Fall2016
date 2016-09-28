0c180b5713f807dd5edf5de9cb08955bf6fe0c46
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-29 01:18:37 +0900

    output code snippets

--- assignments/homework2/Taylor/Taylor/TaylorMain.c
+++ assignments/homework2/Taylor/Taylor/TaylorMain.c
@@ -1,30 +1,79 @@
+#define _USE_MATH_DEFINES
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 // Add your header file
 
 int main(int argc, char* argv[])
 {
-	//Check argument count
-	char errorMsg[] = "usage : Taylor exp(or sin) iter\n";
-	if (argc < 3)
+	double x;
+	int iter;
+	//Check argument count	
+	if (argc == 1)
 	{
-		printf("%s\n", errorMsg);
-		return 0;
+		printf("myexp() example\n");
+		x = 1;
+		iter = 0;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, myexp(x, iter));
+
+		iter = 1;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, myexp(x, iter));
+
+		iter = 2;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, myexp(x, iter));
+
+		iter = 20;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, myexp(x, iter));
+
+		iter = -5;
+		//printf("x = %f, iter = %2d, %f\n", x, iter, myexp(x, iter));
+
+		printf("mysin() example\n");
+		x = M_PI / 2; iter = 2;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, mysin(x, iter));
+
+		iter = 3;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, mysin(x, iter));
+
+		iter = 4;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, mysin(x, iter));
+
+		iter = 5;
+		//printf("x = %f, iter = %2d, %.15f\n", x, iter, mysin(x, iter));
+
+		iter = -3;
+		//printf("x = %f, iter = %2d, %f\n", x, iter, mysin(x, iter));
+
+		return 1;
 	}
 
-	int iter = atoi(argv[2]);
+	if (argc == 2 && (strcmp(argv[1], "output") == 0))
+	{
+		x = 1.0;
+		iter = 20;
+		//double exp_true = exp(x);
+		//double exp_taylor = myexp(x, iter);
+		printf("=== myexp() ===\n");
+		//printf("x = %.15f\n", x);
+		//printf("iterations = %d\n", iter);
+		//printf("exp_true = %.15f\n", exp_true);
+		//printf("exp_tayor = %.15f\n", exp_taylor);
+		//printf("error = %e\n", exp_taylor - exp_true);
 
-	if (strcmp(argv[1], "exp") == 0)
-	{
-		// call myexp()
-	}
-	else if (strcmp(argv[1], "sin") == 0)
-	{
-		// call mysin()
-	}
-	else
-	{
-		printf("%s is not supported\n", argv[1]);
+		printf("\n");
+
+		x = M_PI / 2;
+		iter = 10;
+		printf("=== mysin() ===\n");
+		//double sin_true = sin(x);
+		//double sin_taylor = mysin(x, iter);
+		//printf("x = %.15f\n", x);
+		//printf("iterations = %d\n", iter);
+		//printf("sin_true = %.15f\n", sin_true);
+		//printf("sin_taylor = %.15f\n", sin_taylor);
+		//printf("error = %e\n", sin_taylor - sin_true);
 	}
 
 	return 1;
