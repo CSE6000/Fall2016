c889de71fcdacfecc19deeed0e72e2994a2df17c
HanByul Yang <hanbyul.yang@gmail.com>
2016-11-13 16:48:56 +0900

    added print thread_num

--- codes/OpenMP/Examples/parallel_with_for_loops/parallel_with_for_loops.c
+++ codes/OpenMP/Examples/parallel_with_for_loops/parallel_with_for_loops.c
@@ -22,7 +22,7 @@ int main()
 	#pragma omp parallel
 	#pragma omp for
 	for (i = 0; i< 10; i++)
-		printf("i = %d\n", i);
+		printf("i = %d, thread_num = %d\n", i, omp_get_thread_num());
 
 	return 0;
 }
